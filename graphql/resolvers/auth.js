6c3db18d5eb6d86cbb5b8afc84c3526b4db0199e
Victor <gaeshi@gmail.com>
2019-03-10 21:50:13 +0900

    Add login method

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../../models/user');
 
@@ -25,5 +26,29 @@ module.exports = {
             console.error(e);
             throw e;
         }
+    },
+    login: async ({email, password}) => {
+        const user = await User.findOne({email: email});
+        if (!user) {
+            throw new Error('User does not exist!');
+        }
+
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
+            throw new Error('Password is incorrect!');
+        }
+        const token = jwt.sign(
+            {
+                userId: user.id, email: user.email
+            },
+            'somesupersecretkey',
+            {
+                expiresIn: '1h'
+            });
+        return {
+            userId: user.id,
+            token: token,
+            tokenExpiration: 1
+        };
     }
 };

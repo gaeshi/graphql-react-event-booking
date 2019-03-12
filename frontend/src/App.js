f2baef0bdd972f7d830813a43266864fb3d67b67
Victor <gaeshi@gmail.com>
2019-03-12 20:46:13 +0900

    Implement Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,23 @@ import './App.css';
 import AuthPage from "./pages/Auth";
 import Events from "./pages/Events";
 import Bookings from "./pages/Bookings";
+import MainNavigation from "./components/Navigation/MainNavigation";
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                <Switch>
-                    <Redirect from="/" to="/auth" exact/>
-                    <Route path="/auth" component={AuthPage}/>
-                    <Route path="/events" component={Events}/>
-                    <Route path="/bookings" component={Bookings}/>
-                </Switch>
+                <>
+                    <MainNavigation/>
+                    <main className="main-content">
+                        <Switch>
+                            <Redirect from="/" to="/auth" exact/>
+                            <Route path="/auth" component={AuthPage}/>
+                            <Route path="/events" component={Events}/>
+                            <Route path="/bookings" component={Bookings}/>
+                        </Switch>
+                    </main>
+                </>
             </BrowserRouter>
         );
     }

64fff3a93eaaac3798601d642be35fac49361938
Victor <gaeshi@gmail.com>
2019-03-12 20:23:34 +0900

    Add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
+import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
+
 import './App.css';
+import AuthPage from "./pages/Auth";
+import Events from "./pages/Events";
+import Bookings from "./pages/Bookings";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <h1>It works!</h1>
-            </div>
+            <BrowserRouter>
+                <Switch>
+                    <Redirect from="/" to="/auth" exact/>
+                    <Route path="/auth" component={AuthPage}/>
+                    <Route path="/events" component={Events}/>
+                    <Route path="/bookings" component={Bookings}/>
+                </Switch>
+            </BrowserRouter>
         );
     }
 }
